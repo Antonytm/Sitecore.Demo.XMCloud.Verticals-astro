7eb9dbb03726c723c511bb52116cf864c0a9f7cb
Antonytm <antontishchenko@gmail.com>
2024-12-04 21:37:12 +0200

    Get rid from Angular and Vue

--- src/astro/src/temp/config.js
+++ src/astro/src/temp/config.js
@@ -4,9 +4,9 @@ config.sitecoreApiKey = import.meta.env.SITECORE_API_KEY || '{6D3F291E-66A5-4703
 config.sitecoreApiHost = import.meta.env.SITECORE_API_HOST || 'https://xmcloudcm.localhost';
 config.graphQLEndpointPath = import.meta.env.GRAPH_QL_ENDPOINT_PATH || '/sitecore/api/graph/edge';
 config.graphQLEndpoint = import.meta.env.GRAPH_QL_ENDPOINT || 'https://xmcloudcm.localhost/sitecore/api/graph/edge';
-config.rootItemId = import.meta.env.ROOT_ITEM_ID || '{08D9417D-5071-46FF-896B-EB76B0790526}';
+config.rootItemId = import.meta.env.ROOT_ITEM_ID || '{A13C890B-43EA-45E2-8BAD-A8838112B83D}';
 config.defaultLanguage = import.meta.env.DEFAULT_LANGUAGE || 'en';
 config.fetchWith = import.meta.env.FETCH_WITH || 'GraphQL';
-config.publicUrl = import.meta.env.PUBLIC_URL || 'undefined';
-config.jssAppName = import.meta.env.JSS_APP_NAME || 'Financial';
+config.publicUrl = import.meta.env.PUBLIC_URL || 'services.sxastarter.localhost';
+config.jssAppName = import.meta.env.JSS_APP_NAME || 'Services';
 export default config;
